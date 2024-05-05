bd9a4c451586dce3dd07801484c42439860bf71d
developer.in <152841549+developperin@users.noreply.github.com>
2024-05-05 12:18:44 +0530

    Update SkillPage.jsx

--- WEB SITE/src/components/SkillPage.jsx
+++ WEB SITE/src/components/SkillPage.jsx
@@ -120,7 +120,7 @@ const SkillPage = () => {
                         />
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665 479L12 14zm-4 6v-7.5l4-2.222"
                         />
-                        <h2 className="mt-4 text-xl font-bold text-white text-center mb-4">React</h2>
+                        <h2 className="mt-4 text-xl font-bold text-white text-center mb-4">Figma</h2>
                         <p className="mt-1 text-sm text-gray-300">
                             Figma is a collaborative design tool used for creating user interfaces, wireframes, and prototypes. It offers real-time collaboration, seamless version control, and robust design systems, empowering teams to streamline their design workflows and create visually stunning and interactive digital experiences.
                         </p>
